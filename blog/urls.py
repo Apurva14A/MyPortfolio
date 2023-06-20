31dce52a33e67817174e71a9e6a67c3f433b2fd1
Apurva14A <apooorva01@gmail.com>
2023-06-20 12:49:38 +0530

    Revert "Revert "Add portfoliio website""
    This reverts commit 40a025aaab0689edc2c1c782c05fad0fbe6bfaef.

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from blog import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    
+    path('', views.all_blogs, name='all_blogs'),
+    path('blogs/<int:pk>/', views.blog_detail, name='blog_detail'),
+]
